41dabe1d83cf8f3bd9593a8f35766f1652adcef4
Miron Karchevskii <MironK@radware.com>
2021-03-16 17:53:31 +0200

    merge conflicts

--- src/main/java/com/radware/activitylog/entity/Status1.java
+++
@@ -1,8 +0,0 @@
-//package com.radware.activitylog.entity;
-//
-//public enum Status1 {
-//
-//    FAIL, IN_PROCESS, SUCCESS
-//
-//
-//}
--- src/main/java/com/radware/activitylog/entity/activityType1.java
+++
@@ -1,5 +0,0 @@
-//package com.radware.activitylog.entity;
-//
-//public enum activityType1 {
-//    AAA
-//}
